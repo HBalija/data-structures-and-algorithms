8dcd32f2773906a6f2c900058336689d4b20107a
HBalija <balija.hrvoje@gmail.com>
2019-09-25 20:26:23 +0200

    Add doubly linked list implementation

---
+++ doubly-linked-list/02-doubly-linked-list.js
@@ -0,0 +1,181 @@
+class Node {
+
+  constructor(value) {
+    this.value = value;
+    this.prev = null;
+    this.next = null;
+  }
+}
+
+class DoublyLinkedList {
+
+  constructor() {
+    this.length = 0;
+    this.head = null;
+    this.tail = null;
+  }
+
+  push(val) {
+    const node = new Node(val);
+
+    if (!this.length) {
+      this.head = node;
+      this.tail = node;
+    } else {
+      this.tail.next = node;
+      node.prev = this.tail;
+      this.tail = node;
+    }
+    this.length++;
+    return this;
+  }
+
+  pop() {
+    if (!this.length) return undefined;
+
+    const tail = this.tail;
+
+    if (this.length === 1) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      this.tail = this.tail.prev;
+      this.tail.next = null;
+      tail.prev = null;
+    }
+
+    this.length--;
+    return tail;
+  }
+
+  shift() {
+    if (!this.length) return undefined;
+
+    const head = this.head;
+
+    if (this.length === 1) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      this.head = head.next;
+      this.head.prev = null;
+      head.next = null;
+    }
+
+    this.length--;
+    return head;
+  }
+
+  unshift(val) {
+    const node = new Node(val);
+
+    if (!this.length) {
+      this.head = node;
+      this.tail = node;
+    } else {
+      this.head.prev = node;
+      node.next = this.head;
+      this.head = node;
+    }
+
+    this.length++;
+    return this;
+  }
+
+  get(idx) {
+    if (idx < 0 || idx >= this.length) return null;
+
+    let element, counter;
+
+    if (idx <= this.length / 2) {
+      counter = 0;
+      element = this.head;
+      while (counter < idx) {
+        element = element.next;
+        counter++;
+      }
+    } else {
+      counter = this.length - 1;
+      element = this.tail;
+      while (counter > idx) {
+        element = element.prev;
+        counter--;
+      }
+    }
+    return element;
+  }
+
+  set(idx, val) {
+    const node = this.get(idx);
+
+    if (!node) return false;
+
+    node.value = val;
+    return true;
+  }
+
+  insert(idx, val) {
+    if (idx < 0 || idx > this.length) return false;
+    if (idx === 0) {
+      this.unshift(val);
+    } else if (idx === this.length) {
+      this.push(val);
+    } else {
+      const node = new Node(val);
+      const nodeBefore = this.get(idx - 1);
+
+      nodeBefore.next.previous = node;
+      node.next = nodeBefore.next;
+
+      nodeBefore.next = node;
+      node.previous = nodeBefore;
+
+      this.length++;
+    }
+    return true;
+  }
+
+  remove(idx) {
+    if (idx < 0 || idx >= this.length) return null;
+    if (idx === 0) return this.shift();
+    if (idx === this.length - 1) return this.pop();
+
+    const node = this.get(idx);
+
+    node.prev.next = node.next;
+    node.next.prev = node.prev;
+    node.prev = null;
+    node.next = null;
+
+    this.length--;
+    return node;
+  }
+
+  get str() {
+    if (!this.length) return '[ ]';
+
+    let current = this.head;
+    let items = '[ ';
+    while (current.next) {
+      items += `${current.value}, `;
+      current = current.next;
+    }
+    items += `${this.tail.value} ]`;
+    return items;
+  }
+}
+
+const list = new DoublyLinkedList();
+
+list.push(':');
+list.push('Hello');
+list.push('there');
+
+console.log(list.str);
+console.log('head:', list.head.value);
+console.log('tail: ', list.tail.value);
+
+console.log(list.remove(1));
+console.log(list.str);
+console.log('head:', list.head.value);
+console.log('tail: ', list.tail.value);
---
+++ doubly-linked-list/03-doubly-linked-list.py
@@ -0,0 +1,199 @@
+#!/usr/bin/env python3
+
+
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.prev = None
+        self.next = None
+
+
+class DoublyLinkedList:
+    def __init__(self):
+        self.length = 0
+        self.head = None
+        self.tail = None
+
+    def push(self, val):
+        """
+        Add new item to the end of the list.
+        """
+        node = Node(val)
+
+        if not self.length:
+            self.head = node
+            self.tail = node
+        else:
+            self.tail.next = node
+            node.prev = self.tail
+            self.tail = node
+
+        self.length += 1
+        return self
+
+    def pop(self):
+        """
+        Remove last item from the list.
+        """
+        if not self.length:
+            return None
+
+        tail = self.tail
+
+        if self.length == 1:
+            self.head = None
+            self.tail = None
+        else:
+            self.tail = tail.prev
+            self.tail.next = None
+            tail.prev = None
+
+        self.length -= 1
+        return tail
+
+    def shift(self):
+        """
+        Remove first item from the list.
+        """
+        if not self.length:
+            return None
+
+        head = self.head
+
+        if self.length == 1:
+            self.head = None
+            self.tail = None
+        else:
+            self.head = head.next
+            self.head.prev = None
+            head.next = None
+
+        self.length -= 1
+        return head
+
+    def unshift(self, val):
+        """
+        Add an item to the beginning of the list.
+        """
+        node = Node(val)
+
+        if not self.length:
+            self.head = node
+            self.tail = node
+        else:
+            self.head.prev = node
+            node.next = self.head
+            self.head = node
+
+        self.length += 1
+        return self
+
+    def get(self, idx):
+        """
+        Get the item by its index.
+        """
+        if idx < 0 or idx >= self.length:
+            return None
+
+        element = None
+
+        if idx <= self.length / 2:
+            counter = 0
+            element = self.head
+            while counter < idx:
+                element = element.next
+                counter += 1
+        else:
+            counter = self.length - 1
+            element = self.tail
+            while counter > idx:
+                element = element.prev
+                counter -= 1
+
+        return element
+
+    def set(self, idx, val):
+        """
+        Set the new value for a index.
+        """
+        node = self.get(idx)
+        if not node:
+            return False
+
+        node.value = val
+        return True
+
+    def insert(self, idx, val):
+        """
+        Insert new value on index position.
+        """
+        if idx < 0 or idx > self.length:
+            return False
+
+        if idx == 0:
+            self.unshift(val)
+        elif idx == self.length:
+            self.push(val)
+        else:
+            node = Node(val)
+            node_before = self.get(idx - 1)
+
+            node_before.next.prev = node
+            node.next = node_before.next
+
+            node_before.next = node
+            node.prev = node_before
+
+            self.length += 1
+
+        return True
+
+    def remove(self, idx):
+        """
+        Remove item by index.
+        """
+        if idx < 0 or idx >= self.length:
+            return None
+        if idx == 0:
+            return self.shift()
+        if idx == self.length - 1:
+            return self.pop()
+
+        node = self.get(idx)
+
+        node.prev.next = node.next
+        node.next.prev = node.prev
+        node.next = None
+        node.prev = None
+        self.length -= 1
+        return node
+
+    def __str__(self):
+        if not self.length:
+            return "[ ]"
+
+        current = self.head
+        items = "[ "
+        while current.next:
+            items += "{}, ".format(current.value)
+            current = current.next
+        items += "{} ]".format(self.tail.value)
+
+        return items
+
+
+lst = DoublyLinkedList()
+
+lst.push(":")
+lst.push("Hello")
+lst.push("there")
+
+print(lst)
+print("head:", lst.head.value)
+print("tail: ", lst.tail.value)
+print(lst.length)
+print(lst.remove(1))
+print(lst)
+print("head:", lst.head.value)
+print("tail: ", lst.tail.value)
+print(lst.length)
