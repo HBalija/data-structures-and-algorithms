e981082c7c5e2a36cb5b1787ff84403d0ef8f7b4
HBalija <balija.hrvoje@gmail.com>
2019-10-01 20:40:02 +0200

    Add djikstra pseudocode and implement array priority queue

---
+++ 15-dijkstra-algorithm/04-array-priority-queue.js
@@ -0,0 +1,44 @@
+/*
+Simple priority queue implementation - sorting uses O(n log n)time
+*/
+
+class PriorityQueue {
+
+  constructor() {
+    this.values = [];
+  }
+
+  enqueue(val, priority) {
+    this.values.push({ val, priority });
+    this.sort();
+  }
+
+  dequeue() {
+    return this.values.shift();
+  }
+
+  sort() {
+    this.values.sort((a, b) => a.priority - b.priority);
+  }
+}
+
+
+const q = new PriorityQueue();
+
+q.enqueue('B', 3);
+q.enqueue('C', 5);
+q.enqueue('D', 2);
+q.enqueue('Q', 20);
+q.enqueue('P', 1.5);
+
+console.log(q.values);
+
+/*
+values sorted based on priority
+
+[ { val: 'P', priority: 1.5 },
+  { val: 'D', priority: 2 },
+  { val: 'B', priority: 3 },
+  { val: 'C', priority: 5 },
+  { val: 'Q', priority: 20 } ]
+*/
